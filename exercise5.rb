fa5cfbfa49acd0b89de9e307273e44962545cfb7
MexinaD <mexinamiel@gmail.com>
2014-11-07 00:18:52 +0300

    Converted pounds and inches to kilogram and centimeter respectively

--- exercise5.rb
+++ exercise5.rb
@@ -1,17 +1,21 @@
-name = 'MexinaMiel'
+name = 'Zed A Shaw'
 age = 35 # not a lie in 2009
 height = 74 # inches
 weight = 180 # lbs
 eyes = 'Black'
 teeth = 'White'
 hair = 'Black'
+#from 1inches=2.54cm
+new_height=height*(2.54) #cm
+#from 1pound=0.453kg
+new_weight=weight*(0.453) #kg
 
 puts "Let's talk about #{name}."
-puts "He's #{height} cm tall."
-puts "He's #{weight} Kg heavy."
+puts "He's #{new_height} cm tall."
+puts "He's #{new_weight} Kg heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
