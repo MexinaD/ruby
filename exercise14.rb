a58baacb2527ac319b2dab67582bea15e7d55ba6
Mexina Daniel <mexinamiel@gmail.com>
2015-07-29 14:32:25 +0300

    The prompt name and the symbol used does not bring any effect

--- exercise14.rb
+++ exercise14.rb
@@ -1,15 +1,15 @@
 user_name = ARGV.first
-prompt  = '> '
+give  = '>>> '
 
 puts "Hi #{user_name}."
 puts "I'd like to ask you a few questions."
-puts "Do you like me #{user_name}?", prompt
+puts "Do you like me #{user_name}?", give
 likes = $stdin.gets.chomp
 
-puts "Where do you live #{user_name}?", prompt
+puts "Where do you live #{user_name}?", give
 lives = $stdin.gets.chomp
 
-puts "What kind of computer do you have?", prompt
+puts "What kind of computer do you have?", give
 computer =$stdin.gets.chomp
 
 puts """Alrght, so you said #{likes} about liking me. You live in #{lives}. Not sure whare that is. And you have a #{computer} computer. Nice."""
