c28cbca49a864eff4bba4696888ae2ffa1f82172
Mexina Daniel <mexinamiel@gmail.com>
2015-07-29 15:46:51 +0300

    Prompting the name of the file by using gets.chomp

--- exercise15.rb
+++ exercise15.rb
@@ -1,9 +1,10 @@
-filename = ARGV.first
+#filename = ARGV.first
 
 #This is for opening a file, it return a file 
+puts "Which file you want to read"
+filename = gets.chomp
 txt = open(filename)
 
 puts "Here's your file #{filename}:"
 #this is where the contents of the file are read
 print txt.read
-
