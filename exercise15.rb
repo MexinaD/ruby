39b04211461d88eb640d88088c9347d362250f0b
Mexina Daniel <mexinamiel@gmail.com>
2015-07-29 15:43:33 +0300

    Reading contents of the file once, that user enter the name of the file(with comments)

--- exercise15.rb
+++ exercise15.rb
@@ -7,10 +7,3 @@ puts "Here's your file #{filename}:"
 #this is where the contents of the file are read
 print txt.read
 
-print "Type the filename again: "
-file_again = $stdin.gets.chomp
-# the question is why it has to be the same name, if you change the name it doesnt read the new file contents
-txt_again = open(file_again)
-
-print txt_again.read
-
