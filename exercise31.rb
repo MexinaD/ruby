36cfe0d43043eb01320b91571865ed05e0516834
Mexina Daniel <mexinamiel@gmail.com>
2015-08-17 12:53:11 +0300

    Wrote my own game using if, else, elsif..... nested if

--- exercise31.rb
+++ exercise31.rb
@@ -1,41 +1,32 @@
-puts "You enter a dark room with two doors.  Do you go through door #1 or door #2?"
+puts "Choose the folded paper you want"
+puts "Paper 1"
+puts "Paper 2"
+puts "Paper 3"
+print ">"
+paper = $stdin.gets.chomp
 
-print "> "
-door = $stdin.gets.chomp
-
-if door == "1"
-  puts "There's a giant bear here eating a cheese cake.  What do you do?"
-  puts "1. Take the cake."
-  puts "2. Scream at the bear."
-
-  print "> "
-  bear = $stdin.gets.chomp
-
-  if bear == "1"
-    puts "The bear eats your face off.  Good job!"
-  elsif bear == "2"
-    puts "The bear eats your legs off.  Good job!"
-  else
-    puts "Well, doing %s is probably better.  Bear runs away." % bear
-  end
-
-elsif door == "2"
-  puts "You stare into the endless abyss at Cthulhu's retina."
-  puts "1. Blueberries."
-  puts "2. Yellow jacket clothespins."
-  puts "3. Understanding revolvers yelling melodies."
-
-  print "> "
-  insanity = $stdin.gets.chomp
-
-  if insanity == "1" || insanity == "2"
-    puts "Your body survives powered by a mind of jello.  Good job!"
-  else
-    puts "The insanity rots your eyes into a pool of muck.  Good job!"
-  end
-
+if paper == "1"
+puts "Do you know how many teeth are in dogs' mouth?"
+puts "Answer yes or no"
+print ">"
+respond = $stdin.gets.chomp
+if respond == "yes"
+puts "You are genious"
 else
-  puts "You stumble around and fall on a knife and die.  Good job!"
+puts "Not your fault, go and count"
 end
 
+elsif paper == "2"
+puts "Can you guess how long does an aunt live?"
+puts "Answer yes or no"
+print ">"
+respond1 = $stdin.gets.chomp
+if respond1 == "yes"
+puts "You are genious"
+else
+puts "You didnt pay attention in biology class"
+end
 
+else
+puts "You choose a simple question \n Choose again "
+end
