cbf405d87889366379294da885afa964f3b98032
Mexina Daniel <mexinamiel@gmail.com>
2015-08-18 13:20:00 +0300

    Use of for loop and dot operator

--- exercise33.rb
+++ exercise33.rb
@@ -1,19 +1,17 @@
-def loop(numb1, numb2)
+def loop()
 
-i = 0
+#i = 0
 numbers = []
-while i <= numb1
-  puts "At the top i is #{i}"
-  numbers.push(i)
-
-  i += numb2
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+(0..6).each do |i| #puts i
+numbers.push(i)
 end
+
 puts "The numbers: "
 
 # remember you can write this 2 other ways?
-numbers.each {|num| puts num }
+numbers.each {|i| puts i }
+#i +=2
 end
 
-loop(12 , 3)
+loop()
+
