07403ace5fddeab3f8691383572267bd01afc207
Mexina Daniel <mexinamiel@gmail.com>
2015-08-18 13:54:41 +0300

    You can access the object in the array by using it index, where the indexx start with 0

---
+++ exercise34.rb
@@ -0,0 +1,28 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+puts "I have a list of animals here"
+
+
+  puts "The animal at 1 is #{animals[1]}"
+  puts "The third (3rd) animal is #{animals[2]}"
+  puts "The first (1st) animal is #{animals[0]}"
+  puts "The animal at 3 is #{animals[3]}"
+  puts "The fifth (5th) animal is #{animals[4]}"
+  puts "The animal at 2 is #{animals[2]}"
+  puts "The sixth (6th) animal is #{animals[5]}"
+  puts "The animal at 4 is #{animals[4]}"
+
+#Sentences about the positions of the animals
+ puts "The first (1st) animal is at 0 and is a #{animals[0]}."
+ puts "The second (2nd) animal is at 1 and is a #{animals[1]}."
+ puts "The third (3rd) animal is at 2 and is a #{animals[2]}."
+ puts "The forth (4th) animal is at 3 and is a #{animals[3]}."
+ puts "The fifth (5th) animal is at 4 and is a #{animals[4]}."
+ puts "The sixth (6th) animal is at 5 and is a #{animals[5]}."
+
+#Backward sentences of the above
+ puts "The animal at 0 is the 1st animal and is a #{animals[0]}."
+ puts "The animal at 1 is the 2nd animal and is a #{animals[1]}."
+ puts "The animal at 2 is the 3rd animal and is a #{animals[2]}."
+ puts "The animal at 3 is the 4th animal and is a #{animals[3]}."
+ puts "The animal at 4 is the 5th animal and is a #{animals[4]}."
+ puts "The animal at 5 is the 6th animal and is a #{animals[5]}."
