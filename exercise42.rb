2e66b4397acaa5c7beec490bab082258707e2cd4
MexinaD <mexinamiel@gmail.com>
2015-10-06 23:34:25 +0300

    The ?? are filled with sentences showing tha relationship

--- exercise42.rb
+++ exercise42.rb
@@ -2,29 +2,29 @@
 class Animal
 end
 
-## ??
+## Dog is-a object of Animal
 class Dog < Animal
 
   def initialize(name)
-    ## ??
+    ## Dog has-a name
     @name = name
   end
 end
 
-## ??
+## Cat is-a object of Animal
 class Cat < Animal
 
   def initialize(name)
-    ## ??
+    ## Cat has-a name
     @name = name
   end
 end
 
-## ??
+## Person is-a object
 class Person
 
   def initialize(name)
-    ## ??
+    ## Person has-a name
     @name = name
 
     ## Person has-a pet of some kind
@@ -34,27 +34,27 @@ class Person
   attr_accessor :pet
 end
 
-## ??
+## Employee is-a object of Person
 class Employee < Person
 
   def initialize(name, salary)
-    ## ?? hmm what is this strange magic?
+    ## Employee has-a name
     super(name)
-    ## ??
+    ## Employee has-a salary
     @salary = salary
   end
 
 end
 
-## ??
+## Fish is-a object
 class Fish
 end
 
-## ??
+## Salmon is-a object of Fish
 class Salmon < Fish
 end
 
-## ??
+## Halibut is-a object of Fish
 class Halibut < Fish
 end
 
@@ -62,26 +62,26 @@ end
 ## rover is-a Dog
 rover = Dog.new("Rover")
 
-## ??
+## satan is-a Cat
 satan = Cat.new("Satan")
 
-## ??
+# mary is-a Person
 mary = Person.new("Mary")
 
-## ??
+## mary has-a pet which is-a satan
 mary.pet = satan
 
-## ??
+## frank is-a Employee
 frank = Employee.new("Frank", 120000)
 
-## ??
+## mary is-a Person
 frank.pet = rover
 
-## ??
+## flipper is-a Fish that has-a new 
 flipper = Fish.new()
 
-## ??
+## crouse is-a Halibut that has-a new 
 crouse = Salmon.new()
 
-## ??
+## harry is-a Fish that has-a new 
 harry = Halibut.new()
